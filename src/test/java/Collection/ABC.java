414844fca0e5e30f276fcee7fe3bcaedf7e87814
yogesh12356 <aryannande.an@gmail.com>
2023-04-05 17:17:25 +0530

    ABC

---
+++ src/test/java/Collection/ABC.java
@@ -0,0 +1,20 @@
+package Collection;
+
+public class ABC {
+	public static void main(String[] args) {
+		 
+		int a=0, b=1, c;
+		
+		for(int i=1; i<12; i++)
+		{
+			c=a+b;
+			a=b;
+			b=c;
+			System.out.println(c);	
+			
+					
+			
+		}
+	}
+
+}
--- src/test/java/Collection/Hello.java
+++ src/test/java/Collection/Hello.java
@@ -1,5 +1,9 @@
 package Collection;
 
 public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+	}
+	}
 
-}
