617f48494380f5fd5b00c9eba68d72f244eb62e7
Archu-123 <archanalashkar96@gmail.com>
2023-04-05 17:30:12 +0530

    sabcdef

--- src/test/java/TestNG/DemoNew.java
+++ src/test/java/TestNG/DemoNew.java
@@ -5,7 +5,11 @@ public class DemoNew
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello world");
-		
+		int a=10, b=50, c=0,d=0;
+		c=a+b;
+		d=b/a;
+		System.out.println(c);
+		System.out.println(d);
 	}
 
 }
